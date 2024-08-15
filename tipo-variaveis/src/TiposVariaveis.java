0cefae3f8c4927bed33bc8886d5f83f31a00adee
IPolati <isabelaxavierpolati261098@gmail.com>
2024-08-14 21:51:28 -0300

    variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int soma;
+        int n1 = 5;
+        int n2 = 10;
+
+        soma = n1 + n2;
+
+        System.out.println(soma);
     }
 }
